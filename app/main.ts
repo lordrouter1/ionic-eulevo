258f746f298c9378e05110ed8e9b41b2716b7f61
lordrouter1 <juliobenin@yahoo.com.br>
2019-09-04 11:02:17 -0300

    0.0.1 - primeiro commit

--- app/main.ts
+++ app/main.ts
@@ -9,7 +9,7 @@ platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   if (window['ngRef']) {
     window['ngRef'].destroy();
   }
-  window['ngRef'] = ref;
+  window['ngRef'] = ref; 
 
   // Otherise, log the boot error
 }).catch(err => console.error(err));
